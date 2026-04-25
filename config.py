d6bfa37515379ee39f73e9b6fdef1717152c8758
Alexander Manbeck <ajm9330@psu.edu>
2026-04-25 03:03:43 -0400

    Update config.py
    Changed credentials for DB

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 DB_HOST = "server-3.c4qyicfkvft8.us-east-1.rds.amazonaws.com"
-DB_USER = "root"
-DB_PASS = "rootpassword"
-DB_NAME = "equipment_records"
+DB_USER = "admin"
+DB_PASS = "SamplePass123$"
+DB_NAME = "Equipment Records"
