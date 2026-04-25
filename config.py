fa10681b7a417e4a5b9149bdaabff8d5ef99a8c2
Alexander Manbeck <ajm9330@psu.edu>
2026-04-25 15:05:07 -0400

    Updated RDS Endpoint

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-DB_HOST = "server-3.c4qyicfkvft8.us-east-1.rds.amazonaws.com"
+DB_HOST = "server3.c4qyicfkvft8.us-east-1.rds.amazonaws.com"
 DB_USER = "admin"
 DB_PASS = "SamplePass123$"
 DB_NAME = "Equipment Records"
